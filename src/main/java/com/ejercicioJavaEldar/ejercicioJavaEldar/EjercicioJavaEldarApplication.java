503e63d5a65d2929bfe871e9734f967109c705a4
gnb2000 <barirodriguez@gmail.com>
2021-12-27 14:55:54 -0300

    Initial commit

---
+++ src/main/java/com/ejercicioJavaEldar/ejercicioJavaEldar/EjercicioJavaEldarApplication.java
@@ -0,0 +1,13 @@
+package com.ejercicioJavaEldar.ejercicioJavaEldar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioJavaEldarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioJavaEldarApplication.class, args);
+	}
+
+}
